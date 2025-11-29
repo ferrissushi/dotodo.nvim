44e188c021e74dd77d94bbd5913313a19b14bb74
NickyHariniaina <nickyharini1na@gmail.com>
2025-11-29 15:06:30 +0300

    refactor: create a sep function instead of raw open/close file.

--- lua/dotodo/utils/file_operation.lua
+++ lua/dotodo/utils/file_operation.lua
@@ -3,25 +3,38 @@ local M = {}
 local configuration = require("dotodo.utils.configuration")
 local default_root_dir = configuration.configuration.default_configuration.root_dir
 
+---This is a function to open a file.
+---@param path string The path of the file to open
+---@return integer|string
+local open_file = function(path)
+	if path == nil then
+		return "Path not provided"
+	end
+	local file_content_bufnr = vim.uv.fs_open(path, "r", 438, nil)
+	return file_content_bufnr
+end
+
+---Function to close an opened file
+---@param file_content_bufnr any
+local close_file = function(file_content_bufnr)
+	vim.uv.fs_close(file_content_bufnr)
+end
+
 ---A function to find the TODO.md file in the root of the project.
 ---@return string
 local find_file = function()
 	local todo_file_path = nil
-
 	for _, root_dir in ipairs(default_root_dir) do
 		if todo_file_path ~= nil then
 			break
 		end
-
 		local current_file_path = vim.fs.root(0, root_dir)
-
 		local current_doto_file_path = vim.fs.find({
 			"TODO.md",
 			"todo.md",
 			"TODO.txt",
 			"todo.txt",
 		}, { path = current_file_path, limit = 1, upward = true, type = "file" })
-
 		todo_file_path = current_doto_file_path[1]
 	end
 	return todo_file_path
@@ -31,15 +44,17 @@ end
 ---@param opts any
 M.read_todo_file = function(opts)
 	opts = opts or {}
-
 	local todo_file_path = find_file()
+	return read_file_content(todo_file_path)
+end
 
-	-- Let's open the file...
-	local file_content_bufnr = vim.uv.fs_open(todo_file_path, "r", 438, nil)
-
+---A function to open any file
+---@param path string
+M.read_file_content = function(path)
+	path = path or vim.uv.cwd()
+	local file_content_bufnr = open_file(path)
 	local file_content = vim.uv.fs_read(file_content_bufnr, vim.uv.fs_stat(todo_file_path).size, 0)
-
-	vim.uv.fs_close(file_content_bufnr)
+	close_file(file_content_bufnr)
   return file_content
 end
 
