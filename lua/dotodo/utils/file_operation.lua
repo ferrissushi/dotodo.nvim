00ccdc1470460efe2abf94e870f42cb7b698a264
NickyHariniaina <nickyharini1na@gmail.com>
2025-11-29 12:57:31 +0300

    fix: typo in the todo filename.

--- lua/dotodo/utils/file_operation.lua
+++ lua/dotodo/utils/file_operation.lua
@@ -11,18 +11,28 @@ M.read_file = function(opts)
 	local todo_file_path = nil
 
 	for _, root_dir in ipairs(default_root_dir) do
+		if todo_file_path ~= nil then
+			break
+		end
+
 		local current_file_path = vim.fs.root(0, root_dir)
+
+		-- TODO: Later I should be able to change the TODO file name that I'm searchingo for....
+		-- I should be able to let the user change it...
 		local current_doto_file_path = vim.fs.find({
-			"TODO.md, todo.md, TODO.txt, todo.txt",
-			{ path = current_file_path, limit = 1, upward = true, type = "file" },
-		})
-    local todo_file = current_doto_file_path[1]
-    if todo_file ~= nil then
-      todo_file_path = todo_file
-    end
+			"TODO.md", "todo.md", "TODO.txt", "todo.txt",
+		}, { path = current_file_path, limit = 1, upward = true, type = "file" })
+
+		local todo_file = current_doto_file_path[1]
+
+		if todo_file ~= nil then
+			todo_file_path = todo_file
+		end
 	end
 
-  print(todo_file_path)
+	-- Let's open the file...
+
+	local file_content = vim.uv.fs_open("todo_file_path", "r", 438, nil)
 end
 
 M.read_file()
