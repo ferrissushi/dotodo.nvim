44577d60f2c98329c7dfb0fd97dc96706a8e79f2
NickyHariniaina <nickyharini1na@gmail.com>
2025-11-29 13:05:36 +0300

    refactor: create a find_file function

--- lua/dotodo/utils/file_operation.lua
+++ lua/dotodo/utils/file_operation.lua
@@ -3,11 +3,9 @@ local M = {}
 local configuration = require("dotodo.utils.configuration")
 local default_root_dir = configuration.configuration.default_configuration.root_dir
 
----A function to read the current TODO.md file in the root of the project.
----@param opts any
-M.read_file = function(opts)
-	opts = opts or {}
-
+---A function to find the TODO.md file in the root of the project.
+---@return string|nil
+local find_file = function()
 	local todo_file_path = nil
 
 	for _, root_dir in ipairs(default_root_dir) do
@@ -20,19 +18,31 @@ M.read_file = function(opts)
 		-- TODO: Later I should be able to change the TODO file name that I'm searchingo for....
 		-- I should be able to let the user change it...
 		local current_doto_file_path = vim.fs.find({
-			"TODO.md", "todo.md", "TODO.txt", "todo.txt",
+			"TODO.md",
+			"todo.md",
+			"TODO.txt",
+			"todo.txt",
 		}, { path = current_file_path, limit = 1, upward = true, type = "file" })
 
-		local todo_file = current_doto_file_path[1]
-
-		if todo_file ~= nil then
-			todo_file_path = todo_file
-		end
+		todo_file_path = current_doto_file_path[1]
 	end
+	return todo_file_path
+end
+
+---A function to read the current TODO.md file in the root of the project.
+---@param opts any
+M.read_file = function(opts)
+	opts = opts or {}
+
+  local todo_file_path = find_file()
 
 	-- Let's open the file...
+	local file_content_bufnr = vim.uv.fs_open(todo_file_path, "r", 438, nil)
 
-	local file_content = vim.uv.fs_open("todo_file_path", "r", 438, nil)
+	local file_content = vim.uv.fs_read(file_content_bufnr, vim.uv.fs_stat(todo_file_path).size, 0)
+	print(file_content)
+
+	vim.uv.fs_close(file_content_bufnr)
 end
 
 M.read_file()
