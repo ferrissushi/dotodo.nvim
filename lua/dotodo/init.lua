bf32168b4cb9f121789d0355fc8fca59f8b3a5a4
NickyHariniaina <nickyharini1na@gmail.com>
2025-11-29 12:16:11 +0300

    refactor: send configuration to a separeted file.

--- lua/dotodo/init.lua
+++ lua/dotodo/init.lua
@@ -5,17 +5,6 @@ local M = {}
 local window = require("dotodo.ui.window")
 
 -- Here I just tried to open a new basic plugnis
-local configuration = {
-	relative = "editor",
-	row = math.floor(math.floor(vim.o.lines / 2) / 2),
-	col = math.floor(math.floor(vim.o.columns / 2) / 2),
-	width = math.floor(vim.o.columns / 2),
-	height = math.floor(vim.o.lines / 2),
-	border = "single",
-	style = "minimal",
-	title = "TODOs",
-	title_pos = "left",
-}
 
 M.create_window = function()
 	window.create_todo_list_window({
---
+++ lua/dotodo/utils/configuration.lua
@@ -0,0 +1,14 @@
+local M = {}
+
+M.configuration = {
+	relative = "editor",
+	row = math.floor(math.floor(vim.o.lines / 2) / 2),
+	col = math.floor(math.floor(vim.o.columns / 2) / 2),
+	width = math.floor(vim.o.columns / 2),
+	height = math.floor(vim.o.lines / 2),
+	border = "single",
+	style = "minimal",
+	title = "TODOs",
+	title_pos = "left",
+}
+return M
