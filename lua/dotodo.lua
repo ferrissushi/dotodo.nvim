19f690b4563686b8a92daac67aa6057cacd055ca
NickyHariniaina <nickyharini1na@gmail.com>
2025-11-29 11:54:40 +0300

    feat: remove row number and relative number

--- lua/dotodo.lua
+++ lua/dotodo.lua
@@ -2,12 +2,16 @@ local M = {}
 
 --- Main function to open new window.
 ---@param opts any The configuration of the current window to open.
-create_todo_list_window = function(opts)
+local create_todo_list_window = function(opts)
 	local buffer = vim.api.nvim_create_buf(true, false)
 	local win = vim.api.nvim_open_win(buffer, false, opts.window_configuration)
 	vim.api.nvim_set_current_win(win)
+  vim.opt_local.number = false
+  vim.opt_local.relativenumber = false
+  vim.opt_local.modifiable = false
 end
 
+-- Here I just tried to open a new basic plugnis
 local configuration = {
 	relative = "editor",
 	row = math.floor(math.floor(vim.o.lines / 2) / 2),
